505f03182be6e0dbca7ae8c54ece33b16fc0dd8d
Sanne Stuur <sanne.stuur@gmail.com>
2018-03-14 11:23:09 +0100

    moved guessedSoFar logic to GuessedLetters component en made sure empty word is shown

--- src/components/GuessedLetters.js
+++ src/components/GuessedLetters.js
@@ -1,19 +1,25 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import "./GuessedLetters.css";
-import { addGuessedLetter } from '../actions/addguessedletter'
-
 
 export class GuessedLetters extends Component {
 
-  componentWillMount() {
-    addGuessedLetter(this.props.guesses.guessedLetters)
-  }
 
-  render() {
+    render() {
+
+    let thisguess = this.props.guesses;
+    let answer = thisguess.targetWord.split("");
+
+    for (let i = 0; i < answer.length; i++) {
+      if (thisguess.guessedLetters.includes(answer[i])){
+        thisguess.guessedSoFar.splice(i, 1, answer[i] + " ");
+      }
+      else thisguess.guessedSoFar.splice(i, 1, "_ ");
+    }
+
     return (
       <div className="GuessedLetters">
-        <p> { this.props.guesses.guessedSoFar } </p>
+        <p> { thisguess.guessedSoFar } </p>
       </div>
     );
   }
--- src/reducers/guesses.js
+++ src/reducers/guesses.js
@@ -3,7 +3,7 @@ import { ADD_GUESSED_LETTER } from "../actions/addguessedletter";
 const initialState = {
   targetWord: "kittens",
   guessedLetters: [],
-  guessedSoFar: []
+  guessedSoFar: [],
 };
 
 export default function(state = initialState, action) {
@@ -11,17 +11,7 @@ export default function(state = initialState, action) {
     case ADD_GUESSED_LETTER:
       let thisguess = Object.assign({}, state);
       thisguess.guessedLetters = state.guessedLetters.concat(action.payload);
-
-      let answer = thisguess.targetWord.split("");
-
-      for (let i = 0; i < answer.length; i++) {
-        if (thisguess.guessedLetters.includes(answer[i])){
-          thisguess.guessedSoFar.splice(i, 1, answer[i] + " ");
-        }
-        else thisguess.guessedSoFar.splice(i, 1, "_ ");
-      }
       return thisguess;
-
     default:
       return state;
   }
